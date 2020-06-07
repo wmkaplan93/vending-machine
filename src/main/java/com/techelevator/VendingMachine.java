e43b8e95b207fbcd851d27cf6c6c02d3e627099f
William Kaplan <wmkaplan93@gmail.com>
2020-06-07 13:22:55 -0400

    Some minor balance issues. Also added in a try statement for getMoney. It now has a null entry exception.

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -168,7 +168,7 @@ public class VendingMachine {
 						System.out.println("");
 						System.out.println("What would you like? [B1, C2 (Or type 'Q' to return)] ");
 						System.out.println("");
-						String userLocation = userInput.nextLine();
+						String userLocation = userInput.nextLine().toUpperCase();
 						if ("Q".equalsIgnoreCase(userLocation)) {
 							purchaseMenu();
 						} else if(balance >= Double.parseDouble(productGenerator.get(userLocation).getProductPrice())) {
@@ -179,8 +179,8 @@ public class VendingMachine {
 								System.out.print(" selected. ");
 								productGenerator.get(userLocation).returnMessage();
 								System.out.println("Subtracting " + productGenerator.get(userLocation).getProductPrice() + " from your balance.");
-								System.out.println("Remaining Balance: " + (df.format(balance -= Double.parseDouble(productGenerator.get(userLocation).getProductPrice()))));
 								newBalance = (df.format(balance -= Double.parseDouble(productGenerator.get(userLocation).getProductPrice())));
+								System.out.println("Remaining Balance: " + newBalance);
 								printSalesLog();
 								System.out.println();
 							} else if ((int)inventoryCount.get(userLocation) == 0){
@@ -261,17 +261,20 @@ public class VendingMachine {
 			System.out.println("How much would you like to add? (1, 2, 5, 10 only) ");
 			System.out.println("Or type 'Q' to return to the purchase screen. ");
 			String moreMoney = userInput.nextLine();
-			if ("Q".equalsIgnoreCase(moreMoney)){
-				purchaseMenu();
-			} else if(Integer.parseInt(moreMoney) == 1 || Integer.parseInt(moreMoney) == 2 || Integer.parseInt(moreMoney) == 5 || Integer.parseInt(moreMoney) == 10) {
-				logMoreMoney = Integer.parseInt(moreMoney);
-				balance += Double.parseDouble(moreMoney);
-				System.out.println("Current Balance: " + df.format(balance));
-				printFeedLog();
-			} else {
+			try {
+				if ("Q".equalsIgnoreCase(moreMoney)){
+					purchaseMenu();
+				} else if(Integer.parseInt(moreMoney) == 1 || Integer.parseInt(moreMoney) == 2 || Integer.parseInt(moreMoney) == 5 || Integer.parseInt(moreMoney) == 10) {
+					logMoreMoney = Integer.parseInt(moreMoney);
+					balance += Double.parseDouble(moreMoney);
+					System.out.println("Current Balance: " + df.format(balance));
+					printFeedLog();
+				} else {
+					System.out.println("Please enter a valid whole dollar amount.");
+				}
+			} catch (NumberFormatException e) {
 				System.out.println("Please enter a valid whole dollar amount.");
 			}
-			
 		}
 		
 		//change method

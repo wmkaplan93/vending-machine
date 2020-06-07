3b5e00b652c8c779493f7bd6ee5a7e75f5e79d48
Brody Churovia <bc62@zips.uakron.edu>
2020-06-07 15:32:27 -0400

    Finished testing for change method

--- src/test/java/com/techelevator/VendingMachineTests.java
+++ src/test/java/com/techelevator/VendingMachineTests.java
@@ -14,9 +14,18 @@ public class VendingMachineTests {
 		
 		
 		//Act
-		int[] actualOutput = testSteve.returnChange((Double) null);
+		int[] actualOutput = testSteve.returnChange(0);
 		
 		//Assert
 		Assert.assertArrayEquals(arrays, actualOutput);
 	}
+	
+	@Test
+	public void return_change_actuallyHasMoney() {
+		VendingMachine testSteve = new VendingMachine(null);
+		int[] arrays = new int[] {153 * 4, 0, 0, 0};
+		int[] actualOutput = testSteve.returnChange(153);
+		Assert.assertArrayEquals(arrays, actualOutput);
+	}
+	
 }
